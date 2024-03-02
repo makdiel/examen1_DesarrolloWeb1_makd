f5e82edd4de2ed63accac67603c41371e377f872
makdiel <makdiel.tabora@hotmail.com>
2024-03-01 21:38:01 -0600

    dependencia cros
    agregando dependencias de seguridad

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ import { EncabezadoCompra } from './routes/apiEncabezadoCompra.js';
 import { DetalleCompra } from './routes/apiDetalleCompra.js';
 import { EncabezadoVenta } from './routes/apiEncabezadoVenta.js';
 import { DetalleVenta } from './routes/apiDetalleVenta.js';
+import cors from 'cors';
 
 const app = express();
-
-//middlewares 
-
 app.use(express.json());
+//middlewares 
+const corsOptions = {
+    origin : 'http://localhost:5173', 
+    credentials : true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions));
+
+
 
 const port = 4000;
 
